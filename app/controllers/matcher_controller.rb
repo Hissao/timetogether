9d1de415bccd575910f9e2ab3e50bb22dab5c1f2
Hissao <andre.hissao@youse.com.br>
2017-12-11 22:42:08 -0200

    Fix calculation

--- app/controllers/matcher_controller.rb
+++ app/controllers/matcher_controller.rb
@@ -7,12 +7,15 @@ class MatcherController < ApplicationController
   def show
     @first_name = params[:matcher][:first_name].capitalize.presence || 'Ricardão'
     @second_name = params[:matcher][:second_name].capitalize.presence || 'Tchutchuca'
+
     key = [@first_name, @second_name].sort.sum
     time_together = calculate_time(key).zero? ? 1 : calculate_time(key)
 
     @years = time_together / 360
     @months = time_together % 30 / 12
+    @months = @months.zero? ? 1 : @months
     @days = time_together % 30 % 12
+    @days = @days.zero? && @months.zero? ? 1 : @days
   end
 
   # Try to get first 4 numbers of generated hash
