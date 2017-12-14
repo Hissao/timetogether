e8f3c0e2b4e626e5bc8d288fc6000675a026df66
Hissao <andre.hissao@youse.com.br>
2017-12-14 02:19:01 -0200

    Add Jquery to share buttons js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require jquery_ujs
+//= require social-share-button
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
-//= require social-share-button
